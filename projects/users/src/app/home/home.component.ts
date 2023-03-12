313df4a83bea12a376e90005566117704fe285af
Alejandro Cerezo <alce65@hotmail.es>
2023-03-12 21:10:55 +0100

    ✨ Add Home Module with a component

---
+++ projects/users/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sdi-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+}
