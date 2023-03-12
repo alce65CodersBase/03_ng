e7b77a690f734c997a9b64ab124172e4ef1dbe54
Alejandro Cerezo <alce65@hotmail.es>
2023-03-12 19:34:08 +0100

    ✅ Update test removing wrong importation

--- projects/users/src/app/app.component.spec.ts
+++ projects/users/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { BaseModule } from './base/base.module';
 import { CoreModule } from 'projects/core/src/public-api';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, BaseModule, CoreModule],
+      imports: [RouterTestingModule, CoreModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
