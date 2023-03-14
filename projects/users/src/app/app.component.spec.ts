0a5a3ca878762d911669b42f96be7b68fd98b5f7
Alejandro Cerezo <alce65@hotmail.es>
2023-03-14 21:55:28 +0100

    ♻️ Adapt app to use Layout from core library

--- projects/users/src/app/app.component.spec.ts
+++ projects/users/src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { BaseModule } from './base/base.module';
 import { CoreModule } from 'projects/core/src/public-api';
+import { LayoutComponent } from 'projects/core/src/lib/layout/layout.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, BaseModule, CoreModule],
+      imports: [RouterTestingModule, CoreModule, LayoutComponent],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -16,18 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'users'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Users');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const rendered = fixture.nativeElement as HTMLElement;
-    expect(rendered.querySelector('h1')?.textContent).toContain('Users');
   });
 });
--- projects/users/src/app/app.module.ts
+++ projects/users/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from '../../../core/src/lib/core.module';
-import { BaseModule } from './base/base.module';
+import { LayoutComponent } from 'projects/core/src/lib/layout/layout.component';
+import { CoreModule } from 'projects/core/src/lib/core.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +15,7 @@ import { BaseModule } from './base/base.module';
     AppRoutingModule,
     CoreModule,
     HttpClientModule,
-    BaseModule,
+    LayoutComponent,
   ],
 })
 export class AppModule {}
