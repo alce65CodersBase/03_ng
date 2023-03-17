052278ac1f5961d05aa1d440c43e5aec956b473f
Alejandro Cerezo <alce65@hotmail.es>
2023-03-17 20:09:34 +0100

    ✨ Add User module and component

---
+++ projects/users/src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sdi-user',
+  template: `
+    <p>
+      user works!
+    </p>
+  `,
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+}
