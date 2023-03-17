b67b04ce2f260ff15ced8db5947d267217d5c5be
Alejandro Cerezo <alce65@hotmail.es>
2023-03-17 22:00:28 +0100

    ✅ Test service use in app component and reorder imports

--- projects/users/src/app/about/list/list.component.spec.ts
+++ projects/users/src/app/about/list/list.component.spec.ts
@@ -1,9 +1,9 @@
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { AddComponent } from '../add/add.component';
 import { CardComponent } from '../card/card.component';
-import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
 import { UsersService } from '../services/users.service';
 import { BehaviorSubject } from 'rxjs';
 import { User } from '../../../models/user.model';
--- projects/users/src/app/app.component.spec.ts
+++ projects/users/src/app/app.component.spec.ts
@@ -1,20 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+// import { DebugElement } from '@angular/core';
+// import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from 'projects/core/src/lib/layout/layout.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from './services/user.service';
+
+const noop = () => {
+  // No operations
+};
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  // let debugElement: DebugElement;
+  let service: UserService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, LayoutComponent, HttpClientTestingModule],
       declarations: [AppComponent],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            loadProfile: noop,
+          },
+        },
+      ],
     }).compileComponents();
+
+    service = TestBed.inject(UserService);
+    spyOn(service, 'loadProfile').and.callThrough();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    // debugElement = fixture.debugElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should use service', () => {
+    component.handleProfile();
+    expect(service.loadProfile).toHaveBeenCalled();
   });
 });
