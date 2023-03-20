bae7c90903e97d7ef167fc38e67033beff0734af
Alejandro Cerezo <alce65@hotmail.es>
2023-03-20 20:07:36 +0100

    Add Lessons to home component

--- projects/courses/src/app/home/home.component.spec.ts
+++ projects/courses/src/app/home/home.component.spec.ts
@@ -1,23 +1,34 @@
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { MaterialModule } from '../material.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [MaterialModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should click for select a lesson', () => {
+    spyOn(component, 'selectLesson').and.callThrough();
+    const debugButtons = debugElement.queryAll(By.css('[role="button"]'));
+    debugButtons[0].triggerEventHandler('click');
+    expect(component.selectLesson).toHaveBeenCalled();
   });
 });
--- projects/courses/src/app/home/home.component.ts
+++ projects/courses/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Lesson } from '../../models/lessons';
+import { CoursesRepoService } from '../services/courses.repo.service';
 
 @Component({
   selector: 'sdi-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  lessons: Lesson[];
+  selectedLesson: Lesson | null;
+  constructor(private repoSrv: CoursesRepoService) {
+    this.lessons = [];
+    this.repoSrv
+      .getLessonsMock()
+      .subscribe((lessons) => (this.lessons = lessons));
+    this.selectedLesson = this.lessons[0];
+  }
 
+  selectLesson(lesson: Lesson) {
+    this.selectedLesson = lesson;
+  }
 }
