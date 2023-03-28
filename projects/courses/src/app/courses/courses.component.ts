13d592e571f07881944fcff3755b682364ac24bc
Alejandro Cerezo <alce65@hotmail.es>
2023-03-28 22:53:29 +0200

    👔 Update Courses component with new repo functionality

--- projects/courses/src/app/courses/courses.component.ts
+++ projects/courses/src/app/courses/courses.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../../models/courses';
-import { CoursesRepoService } from '../services/courses.repo.service';
+import { CoursesApiRepoService } from '../services/courses.api.repo.service';
 
 const initialCourse: Course = {
-  id: 0,
+  id: '',
   title: '',
   description: '',
   percentComplete: 0,
@@ -20,16 +20,25 @@ export class CoursesComponent implements OnInit {
   selectedCourse!: Course | null;
   title!: string;
 
-  constructor(private repoSrv: CoursesRepoService) {
+  constructor(private repoSrv: CoursesApiRepoService) {
     this.courses = [];
     this.selectedCourse = initialCourse;
   }
 
   ngOnInit(): void {
-    this.repoSrv.getCoursesMock().subscribe((courses) => {
-      this.courses = courses;
-      this.selectedCourse = { ...this.courses[0] };
-      this.title = this.selectedCourse.title;
+    this.getAllCourses();
+  }
+
+  getAllCourses() {
+    this.repoSrv.loadItems().subscribe({
+      next: (courses) => {
+        this.courses = courses;
+        this.selectedCourse = { ...this.courses[0] };
+        this.title = this.selectedCourse.title;
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
@@ -37,6 +46,7 @@ export class CoursesComponent implements OnInit {
     this.selectedCourse = { ...course } as Course;
     this.title = this.selectedCourse.title;
   }
+
   deleteCourse(ev: Event, id: Course['id']) {
     ev.stopImmediatePropagation();
     this.courses = this.courses.filter((course) => course.id !== id);
@@ -45,7 +55,43 @@ export class CoursesComponent implements OnInit {
   }
 
   saveCourse() {
-    console.log(this.selectedCourse);
+    if (this.selectedCourse?.id) {
+      console.log('Update');
+      this.updateCourse();
+    } else {
+      console.log('Add');
+      this.addCourse();
+    }
+  }
+
+  updateCourse() {
+    if (!this.selectedCourse) return;
+    this.repoSrv.updateItem(this.selectedCourse).subscribe({
+      next: (course) => {
+        this.courses = this.courses.map((item) =>
+          item.id === course.id ? course : item
+        );
+        this.selectedCourse = { ...course };
+        this.title = this.selectedCourse.title;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  addCourse() {
+    if (!this.selectedCourse) return;
+    this.repoSrv.createItem(this.selectedCourse).subscribe({
+      next: (course) => {
+        this.courses = [...this.courses, course];
+        this.selectedCourse = { ...(this.courses.at(-1) as Course) };
+        this.title = this.selectedCourse.title;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   resetCourse() {
--- projects/courses/src/app/home/home.module.ts
+++ projects/courses/src/app/home/home.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../material.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, MaterialModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+    HomeRoutingModule,
+    CoreModule,
+  ],
 })
 export class HomeModule {}
