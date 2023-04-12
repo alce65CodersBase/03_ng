4b899d7d3a18614dc801bd8093046f7584d24a69
Alejandro Cerezo <alce65@hotmail.es>
2023-04-12 16:47:28 +0200

    ♻️ Add two components for split Courses component

---
+++ projects/courses/src/app/courses2/courses-details/courses-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sdi-courses-details',
+  templateUrl: './courses-details.component.html',
+  styleUrls: ['./courses-details.component.scss']
+})
+export class CoursesDetailsComponent {
+
+}
--- projects/courses/src/app/courses2/courses2.module.ts
+++ projects/courses/src/app/courses2/courses2.module.ts
@@ -4,9 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { Courses2RoutingModule } from './courses2-routing.module';
 import { CoursesComponent } from './courses.component';
 import { MaterialModule } from '../material.module';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesDetailsComponent } from './courses-details/courses-details.component';
 
 @NgModule({
-  declarations: [CoursesComponent],
+  declarations: [
+    CoursesComponent,
+    CoursesListComponent,
+    CoursesDetailsComponent,
+  ],
   imports: [CommonModule, FormsModule, Courses2RoutingModule, MaterialModule],
 })
 export class Courses2Module {}
