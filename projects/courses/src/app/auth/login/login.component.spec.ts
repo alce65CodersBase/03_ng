68490954511f0c90e4bae110e526b1da9cbdad4a
Alejandro Cerezo <alce65@hotmail.es>
2023-04-14 16:52:49 +0200

    ♻️ Refactor login component using auth service

--- projects/courses/src/app/auth/login/login.component.spec.ts
+++ projects/courses/src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [HttpClientModule, ReactiveFormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
